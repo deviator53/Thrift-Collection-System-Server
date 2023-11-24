85bb1b667790596863ca858488394ac01226a675
David Bradford <davidbradforduko@yahoo.com>
2023-11-24 14:47:02 +0100

    single user route added

--- api/server.js
+++ api/server.js
@@ -20,7 +20,7 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+    '/users/:id': '/users/:id',
 }))
 server.use(router)
 server.listen(3000, () => {
